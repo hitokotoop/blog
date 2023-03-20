14334346700f8be6daf3c4407bc527d736909844
hitokotoop <hitokotoop@outlook.com>
2023-03-20 22:40:06 +0800

    add-code

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
-export const SITE_TITLE = `Austin's Blog`;
+export const SITE_TITLE = `Hiko's Blog`;
 export const SITE_DESCRIPTION = 'Austin Site Description';
 export const SITE_EMAIL = 'no.sql@qq.com'
 export const SITE_NAME = 'astro.yufengbiji.com';
