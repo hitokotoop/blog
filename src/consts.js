d8aa29b070a01fa0e82481981576138f5948b52f
hikochen <hiko-chen@outlook.com>
2023-03-21 10:21:52 +0800

    add code

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `Hiko's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_DESCRIPTION = '一些随想记录';
+export const SITE_EMAIL = 'hiko-chen@outlook.com'
+export const SITE_NAME = 'hikoio.com';
+export const SITE_URL = "https://hikoio.com/";
