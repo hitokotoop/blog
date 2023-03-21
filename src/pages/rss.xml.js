1e5b0024c1b1b072674745bda013370c5749c888
hitokotoop <hitokotoop@outlook.com>
2023-03-21 10:38:52 +0800

    add code

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,9 +2,9 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function get() {
   return rss({
-    title: "Austin's Blog",
-    description: "Site description",
-    site: 'https://astro-blog.qum.cc',
+    title: "Hikoio",
+    description: "Nothing notable",
+    site: 'https://hikoio.com',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>zh-cn</language>`,
   });
