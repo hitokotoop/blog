099fed8357b18c6f7f5fb0d93ac1a1537a3ac607
hitokotoop <hitokotoop@outlook.com>
2023-03-21 11:12:50 +0800

    add code

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Hiko's Blog`;
-export const SITE_DESCRIPTION = '一些随想记录';
+export const SITE_TITLE = `Hikoio`;
+export const SITE_DESCRIPTION = 'Nothing notable';
 export const SITE_EMAIL = 'hiko-chen@outlook.com'
 export const SITE_NAME = 'hikoio.com';
 export const SITE_URL = "https://hikoio.com/";
