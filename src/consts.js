bb2bfd8b3c8dae459887d3e09a076bef26855339
hitokotoop <hitokotoop@outlook.com>
2023-03-21 15:52:48 +0800

    change icon

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `Hikoio`;
 export const SITE_DESCRIPTION = 'Nothing notable';
-export const SITE_EMAIL = 'hiko-chen@outlook.com'
+export const SITE_EMAIL = 'hitokotoop@hotmail.com'
 export const SITE_NAME = 'hikoio.com';
 export const SITE_URL = "https://hikoio.com/";
